d594f1b186a279c49df5c1a9ed375ed4dc8e84b8
Anas-Jabr <64369345+Anas-Jabr@users.noreply.github.com>
2020-11-24 21:42:25 +0200

    Update AKMaskFieldUtility.swift

--- AKMaskField/AKMaskFieldUtility.swift
+++ AKMaskField/AKMaskFieldUtility.swift
@@ -61,7 +61,7 @@ public class AKMaskFieldUtility {
     public class func matchesInString(_ string: String, pattern: String) -> [NSTextCheckingResult] {
         return  try!
             NSRegularExpression(pattern: pattern, options: .caseInsensitive)
-                .matches(in: string, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, string.characters.count))
+                .matches(in: string, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, string.count))
     }
     
     public class func findIntersection(_ ranges: [NSRange], withRange range: NSRange) -> [NSRange?] {
